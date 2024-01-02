e725c38325608b665697f1b51a6921d06f528f92
nyaga <c1223781@st.kanazawa-it.ac.jp>
2024-01-02 23:59:03 +0900

    feat: ImageのSizeを指定

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,6 +17,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
@@ -44,10 +47,13 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun NameSection(){
+fun NameSection(modifier: Modifier = Modifier){
     val image = painterResource(id = R.drawable.android_logo)
     Column {
-        Image(painter = image, contentDescription = null)
+        Image(painter = image, contentDescription = null,
+            modifier = Modifier
+                .size(100.dp)
+        )
         Text(text = "Full Name",
             fontSize = 16.sp
             )
