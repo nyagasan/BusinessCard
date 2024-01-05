67a41a466112881a044f52e783643f4382c8c2ae
nyaga <c1223781@st.kanazawa-it.ac.jp>
2024-01-05 23:54:43 +0900

    feat: アライメントを追加

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -66,7 +67,7 @@ fun NameSection(modifier: Modifier = Modifier){
 
 @Composable
 fun IconText(){
-    Row {
+    Row(horizontalArrangement = Arrangement.SpaceBetween) {
         Icon(
             imageVector = Icons.Default.Call,
             contentDescription = "Call Icon"
